ab4a69b894fb584f2c0b97262b7f48de6db8cb65
Macai, Mario <mario.macai@accenture.com>
2022-11-22 16:50:37 +0100

    exchanged unittest with pytest and added pre-push setup

---
+++ tests/test_validations.py
@@ -0,0 +1,16 @@
+"""Test cases for the validation module"""
+import pytest
+
+from src.goechargerv2.validations import validate_empty_string
+
+
+def test_validation_ok() -> None:
+    """Test if a non empty string is valid, thus returns None"""
+    assert validate_empty_string("test", "hello") == None
+
+
+def test_validation_error() -> None:
+    """Test if an empty string is raises an error"""
+    with pytest.raises(Exception) as exception_info:
+        validate_empty_string("", "hello")
+    assert str(exception_info.value) == "hello must be specified"
